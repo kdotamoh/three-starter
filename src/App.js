1a7fd9eb7039f38d0cc7ee53778f25d3655099ad
kwadwo-amoh <21177558+kdotamoh@users.noreply.github.com>
2019-03-23 22:46:49 +0000

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three';
 
 class App extends Component {
+  componentDidMount() {
+    const WIDTH = this.mount.clientWidth;
+    const HEIGHT = this.mount.clientHeight;
+
+    // ADD SCENE
+    this.scene = new THREE.Scene()
+
+    // ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      WIDTH / HEIGHT,
+      0.1,
+      1000
+    )
+    this.camera.position.set(0, 0, 30)
+
+    // ADD RENDERER
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setClearColor(0xEEEEEE);
+    this.renderer.setSize(WIDTH, HEIGHT);
+    this.mount.appendChild(this.renderer.domElement)
+
+    // ADD CUBE
+    const geo = new THREE.BoxGeometry(4, 4, 4);
+    const mat = new THREE.MeshBasicMaterial({ color: '#BADA55'})
+
+    this.cube = new THREE.Mesh(geo, mat)
+    this.scene.add(this.cube)
+
+    this.start()
+  }
+
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate)
+    }
+  }
+
+  stop = () => {
+    cancelAnimationFrame(this.frameId)
+  }
+
+  animate = () => {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderScene()
+    this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  renderScene = () => {
+    this.renderer.render(this.scene, this.camera)
+  }
+
   render() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div
+      style={{width, height}}
+        ref={(mount) => this.mount = mount}
+      />
     );
   }
 }
